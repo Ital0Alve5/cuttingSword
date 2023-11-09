5d495b47df48e0bfd954b46ad8f4f0593542b7f5
Ital0Alve5 <italo_alves_@outlook.com>
2023-11-09 00:57:16 -0300

    initial mvc structure

--- controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HomeController
-{
-    public function index()
-    {
-        require("./view/home.php");
-    }
-}
--- view/components/footer.php
+++
@@ -1,4 +0,0 @@
-<script src="../../assets/js/index.js"></script>
-</body>
-
-</html>
