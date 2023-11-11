4e6ebb7a03037c5cbad5718c002fea6a82c730b7
Ital0Alve5 <italo_alves_@outlook.com>
2023-11-11 13:36:36 -0300

    adjust back button and column names

--- view/history.php
+++ view/history.php
@@ -11,17 +11,20 @@ include './view/components/header.php';
 ?>
 
 <div class="historyContainer">
-    <h1>Histórico do jogo</h1>
+    <div class="header">
+        <a href="/">❮</a>
+        <h1>Histórico do jogo</h1>
+    </div>
     <div class="tableContainer">
         <table>
             <thead>
                 <th>Usuário</th>
                 <th>Modo</th>
-                <th>Vitória</th>
-                <th>Nível</th>
-                <th>Pontos</th>
-                <th>Tempo restante</th>
-                <th>Data</th>
+                <th>Resultado</th>
+                <th>Dificuldade</th>
+                <th>Pontuação</th>
+                <th>Tempo Restante</th>
+                <th>Data e Hora</th>
             </thead>
             <tbody></tbody>
         </table>
