045f1d89495bd621a03a00f232f9b81a47a44f6f
PaG0Dezaum <heitor.hes@hotmail.com>
2023-11-12 00:07:42 -0300

    facilitei seu teste, se vc entrar em /league logado n deve acontecer nada, se entrar sem login voce deve ser redirecionado para pagina /entry

--- controllers/LeagueController.php
+++ controllers/LeagueController.php
@@ -2,8 +2,10 @@
 
 class LeagueController
 {
+
     public function getLeagueNameByLeagueId($params)
     {
+        SessionController::SessionProtection();
         $league = new Leagues();
         $league->loadLeagueById($params['leagueId']);
         echo json_encode(["leagueName" => $league->getName()]);
