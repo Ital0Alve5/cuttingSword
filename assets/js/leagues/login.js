67cc2f6ebb434b7c870c2eaee2ac1a0ad71a01bf
Ital0Alve5 <italo_alves_@outlook.com>
2023-11-15 16:33:40 -0300

    create errors in league entry

---
+++ assets/js/leagues/login.js
@@ -0,0 +1,50 @@
+import {
+  isEmpty,
+  isPasswordInvalid,
+  isUsernameInvalid,
+} from "../utils/sanitize.js";
+
+import { showError, hideError } from "../utils/setError.js";
+
+const leagueNameField = document.querySelector(".loginLeague .emailField");
+const passwordField = document.querySelector("#loginForm .passwordField");
+const loginLeagueButton = document.querySelector("#loginForm button");
+
+const hasErrors = () => {
+  if (
+    isEmpty([
+      { element: emailField, label: "Email" },
+      { element: passwordField, label: "Senha" },
+    ])
+  )
+    return true;
+  else if (isEmailInvalid(emailField)) return true;
+  else if (isPasswordInvalid(passwordField)) return true;
+  return false;
+};
+
+export const handleLogin = () => {
+  emailField.value = emailField.value.trim();
+  passwordField.value = passwordField.value.trim();
+
+  loginButton.addEventListener("click", async () => {
+    hideError();
+
+    if (hasErrors()) return;
+
+    const login = await fetch("http://127.0.0.1:5200/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: emailField.value,
+        password: passwordField.value,
+      }),
+    });
+    const loginJson = await login.json();
+
+    if (!loginJson.error) {
+      location.replace("/game");
+    } else {
+      showError(loginJson.message, [emailField, passwordField]);
+    }
+  });
+};
