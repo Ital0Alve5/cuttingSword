d7a54cfc50315c64cfbb8b08a775ffdd59547afa
Ítalo Alves de Paula e Silva <71088021+Ital0Alve5@users.noreply.github.com>
2023-11-15 21:55:30 -0300

    Update controllers/api/GameController.php

--- controllers/api/GameController.php
+++ controllers/api/GameController.php
@@ -10,7 +10,7 @@ use models\LeagueGameHistory;
 class GameController extends Controller{
     public function index($data){
 
-        if($data['leagueId' == 0]) {
+        if($data['leagueId'] == 0) {
             CasualGameHistory::createUserHistory($data['userId'], $data['timeLeft'], $data['victory'], $data['matchPoints'], $data['matchLevel']);
         } else {
             LeagueGameHistory::createLeagueUserHistory($data['leagueId'], $data['userId'], $data['timeLeft'], $data['victory'], $data['matchPoints'], $data['matchLevel']);
