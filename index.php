a2f90375d0147133c8ccb4f8582dd5fd99dd15e7
Ital0Alve5 <italo_alves_@outlook.com>
2023-11-28 21:32:20 -0300

    remove unnecessary autoloads

--- index.php
+++ index.php
@@ -8,13 +8,7 @@ spl_autoload_register(
             if (file_exists(__DIR__ . '/' . $className . '.php')) {
                 require_once(__DIR__ . '/' . $className . '.php');
             }
-        } else if (file_exists(__DIR__ . '/controllers/' . $className . '.php')) {
-            require_once(__DIR__ . '/controllers/' . $className . '.php');
-        } else if (file_exists(__DIR__ . '/models/' . $className . '.php'))
-            require_once(__DIR__ . '/models/' . $className . '.php');
-        else if (file_exists(__DIR__ . '/models/db/' . $className . '.php'))
-            require_once(__DIR__ . '/models/db/' . $className . '.php');
-        else if (file_exists(__DIR__ . '/app/' . $className . '.php')) {
+        } else if (file_exists(__DIR__ . '/app/' . $className . '.php')) {
             require_once(__DIR__ . '/app/' . $className . '.php');
         }
     }
